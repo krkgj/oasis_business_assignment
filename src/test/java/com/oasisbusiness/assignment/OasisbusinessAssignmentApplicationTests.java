87d2e94be7ad5880325f44d3607c7461aede7966
Kyeongjin Kim <krkgj0526@naver.com>
2022-01-09 23:27:55 +0900

    initialization

---
+++ src/test/java/com/oasisbusiness/assignment/OasisbusinessAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasisbusiness.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OasisbusinessAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
